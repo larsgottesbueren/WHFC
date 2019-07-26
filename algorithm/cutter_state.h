465c1f39136280ca8c9191625c29d30b401d3cde
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2019-07-26 15:40:19 +0200

    commodity in cutter_state

--- algorithm/cutter_state.h
+++ algorithm/cutter_state.h
@@ -14,7 +14,7 @@ namespace whfc {
 		using Pin = FlowHypergraph::Pin;
 
 		int viewDirection = 0;
-		FlowHypergraph& flow_hg;
+		FlowHypergraph& hg;
 		Flow cutSize = 0;
 
 		using ReachableNodes = BitsetReachableNodes;
@@ -29,17 +29,17 @@ namespace whfc {
 		NodeBorder borderNodes;
 		IsolatedNodes isolatedNodes;
 
-		inline bool isIsolated(const Node u) const { return !n.isSource(u) && !n.isTarget(u) && isolatedNodes.mixedIncidentHyperedges[u] == flow_hg.degree(u); }
+		inline bool isIsolated(const Node u) const { return !n.isSource(u) && !n.isTarget(u) && isolatedNodes.isCandidate(u); }
 		inline bool canBeSettled(const Node u) const { return !n.isSource(u) && !n.isTarget(u) && !isIsolated(u); }
 
 		inline bool hasSourcePin(const Hyperedge e) const { return cut.hasSettledSourcePins[e]; }
 		inline bool hasTargetPin(const Hyperedge e) const { return cut.hasSettledTargetPins[e]; }
 
-		inline bool shouldBeAddedToCut(const Hyperedge e) const { return !h.areAllPinsSourceReachable(e) && !cut.wasAdded(e) && flow_hg.isSaturated(e); }	//the first condition is just an optimization, not really necessary
+		inline bool shouldBeAddedToCut(const Hyperedge e) const { return !h.areAllPinsSourceReachable(e) && !cut.wasAdded(e) && hg.isSaturated(e); }	//the first condition is just an optimization, not really necessary
 
 		inline void addToCut(const Hyperedge e) {
 			assert(shouldBeAddedToCut(e));
-			for (const Pin& px : flow_hg.pinsOf(e))
+			for (const Pin& px : hg.pinsOf(e))
 				if (canBeSettled(px.pin) && !borderNodes.wasAdded(px.pin))
 					borderNodes.add(px.pin);
 			cut.add(e);
@@ -51,16 +51,16 @@ namespace whfc {
 				n.reach(u);
 			n.settle(u);
 
-			for (const auto& he_inc : flow_hg.hyperedgesOf(u)) {
+			for (const auto& he_inc : hg.hyperedgesOf(u)) {
 				const Hyperedge e = he_inc.e;
 				if (!hasSourcePin(e)) {
 					cut.hasSettledSourcePins.set(e);
 					if (hasTargetPin(e)) {	//e just became mixed
-						for (const Pin& px : flow_hg.pinsOf(e)) {
+						for (const Pin& px : hg.pinsOf(e)) {
 							const Node p = px.pin;
 							isolatedNodes.mixedIncidentHyperedges[p]++;
 							/*
-							 * Previously, we identified candidates, via mixedIncidentHyperedges[p] == flow_hg.degree(p), and later accepted them as isolated, only if they were not settled.
+							 * Previously, we identified candidates, via mixedIncidentHyperedges[p] == hg.degree(p), and later accepted them as isolated, only if they were not settled.
 							 * This was particularly necessary for piercing hyperedges.
 							 * However, it is suboptimal since the later settled candidates could just as well be moved between the blocks without modifying the cut.
 							 * Immediately isolating them can yield better final cuts and is substantially less code.
@@ -68,7 +68,7 @@ namespace whfc {
 							 * However, we have to move the ones that were marked reachable out of that set. Also make sure that it is IMPOSSIBLE to actually ever reach an isolated node.
 							 */
 							if (isIsolated(p))
-								isolatedNodes.add(p, flow_hg.nodeWeight(p));
+								isolatedNodes.add(p);
 						}
 					}
 				}
@@ -84,12 +84,28 @@ namespace whfc {
 			borderNodes.flipViewDirection();
 		}
 
-		int currentViewDirection() const { return viewDirection; }
-		int oppositeViewDirection() const { return 1 - viewDirection; }
+		int currentViewDirection() const {
+			return viewDirection;
+		}
+
+		int oppositeViewDirection() const {
+			return 1 - viewDirection;
+		}
 
 		void clearForSearch() {
-			n.resetSourceReachableToSource();
-			h.resetSourceReachableToSource();
+			if (augmentingPathAvailable) {
+				n.resetSourceReachableToSource();
+				h.resetSourceReachableToSource();
+			}
+		}
+
+		void filterBorder() {
+			borderNodes.filter([&](const Node& x) { return !canBeSettled(x);} );
+		}
+
+		void filterCut() {
+			//not necessary at the moment
+			//cut.deleteNonCutHyperedges(h);
 		}
 
 	};
