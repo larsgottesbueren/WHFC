124d6d11fc26b79f5f45bc8faaa4d012dd08b81f
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2019-08-12 18:29:37 +0200

    commodity stuff for queue

--- datastructure/queue.h
+++ datastructure/queue.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include "../util/range.h"
+#include <boost/range/irange.hpp>
 
 template<typename T, typename queue_size_type=uint32_t>
 class LayeredQueue {
@@ -11,7 +12,8 @@ public:
 	using size_type = queue_size_type;
 	size_type layerfront, layerend, qfront, qend;
 	explicit LayeredQueue(const size_type num_elements) : queue(num_elements), layerfront(0), layerend(0), qfront(0), qend(0) {}
-	inline void clear() { layerfront = 0; layerend = 0; qfront = 0; qend = 0; }
+	inline void reinitialize(size_type x) { layerfront = x; layerend = x; qfront = x; qend = x; }
+	inline void clear() { reinitialize(0); }
 	inline bool empty() const { return qfront == qend; }
 	inline bool currentLayerEmpty() const { return qfront == layerend; }
 	inline T pop() { return queue[qfront++]; }
@@ -22,9 +24,19 @@ public:
 	inline T capacity() const { return static_cast<T>(queue.size()); }
 	inline std::vector<T>& data() { return queue; }
 	template<typename Func> inline void forAllEverContainedElements(Func f) { for (size_type i = 0; i < qend; i++) { f(queue[i]); } }
-	inline const_range<T> range(size_type __begin, size_type __end) { return { queue.begin() + __begin, queue.cbegin() + __end }; }
-	inline const_range<T> currentLayer() { return range(layerfront, qend); }
+	inline const_range<std::vector<T>> range(size_type __begin, size_type __end) { return { queue.begin() + __begin, queue.cbegin() + __end }; }
+	inline const_range<std::vector<T>> currentLayer() { return range(qfront, layerend); }
+	inline const_range<std::vector<T>> allElements() { return range(0, qend); }
 	inline size_type queueEnd() const { return qend; }
+	inline T popBack() { Assert(qend > 0); return queue[--qend]; }
+
+	inline decltype(auto) currentLayerIndices() { return boost::irange<size_type>(qfront, layerend); }
+	inline void truncateQueue(size_type new_end) {
+		qend = std::min(new_end, qend);
+		layerend = std::min(new_end, layerend);
+		qfront = std::min(new_end, qfront);
+		layerfront = std::min(new_end, layerfront);
+	}
 
 	inline T elementAt(const size_type pos) const { return queue[pos]; }
 	inline void setTo(const size_type pos, T element) { queue[pos] = element; }
@@ -36,9 +48,9 @@ public:
 
 	std::vector<T> extract() { return std::move(queue); }
 
-	template<typename URNG>
-	void shuffleNextLayer(URNG&& urng) {
-		std::shuffle(queue.begin() + layerfront, queue.begin() + layerend, urng);
+	template<typename URBG>
+	void shuffleCurrentLayer(URBG &&urbg) {
+		std::shuffle(queue.begin() + layerfront, queue.begin() + layerend, urbg);
 	}
 
 	template<bool resize = true>
