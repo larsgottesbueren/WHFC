7e95696fe6adc6093c7ebc21ccacf4590e3686a9
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2019-10-28 08:50:49 +0100

    remove comment

--- util/custom_asserts.h
+++ util/custom_asserts.h
@@ -8,8 +8,6 @@ inline void ensure(const bool assumption) noexcept {
     if (!assumption) exit(1);
 }
 
-//#define Assert(assumption) assert(assumption)
-//These don't seem necessary anymore, since we not get appropriate line numbers and filenames
 #define AssertMsg(assumption, msg) assert(( (void)(msg), (assumption) ))
 #define Assert(assumption) assert((assumption))
 
