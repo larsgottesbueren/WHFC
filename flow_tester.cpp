a942f235aaafd1191584e0130ac52a2e83334d56
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-11-03 12:18:10 +0100

    add executable for testing flow algorithms without invoking all of the flowcutter framework

---
+++ flow_tester.cpp
@@ -0,0 +1,98 @@
+#include <queue>
+#include "datastructure/flow_hypergraph.h"
+#include "io/hmetis_io.h"
+#include "io/whfc_io.h"
+#include "datastructure/flow_hypergraph_builder.h"
+#include "algorithm/dinic_bidirectional.h"
+#include "algorithm/dinic.h"
+#include "algorithm/dinic_scaling.h"
+
+
+namespace whfc {
+	
+	template<typename FlowAlgorithm>
+	int runSnapshotTester(const std::string& filename) {
+		static constexpr bool log = true;
+		
+		WHFC_IO::WHFCInformation info = WHFC_IO::readAdditionalInformation(filename);
+		Node s = info.s;
+		Node t = info.t;
+		std::cout << s << " " << t << " " << info.maxBlockWeight[0] << " " << info.maxBlockWeight[1] << " " << info.upperFlowBound<< std::endl;
+		
+		FlowHypergraphBuilder hg = HMetisIO::readFlowHypergraphWithBuilder(filename);
+		if (s >= hg.numNodes() || t >= hg.numNodes())
+			throw std::runtime_error("s or t not within node id range");
+		
+		LOGGER << V(hg.numNodes()) << V(hg.numHyperedges()) << V(hg.numPins());
+		
+		FlowAlgorithm flow_algo(hg);
+		TimeReporter timer;
+		CutterState<FlowAlgorithm> cs(hg, timer);
+		for (int i = 0; i < 2; ++i)
+			cs.setMaxBlockWeight(i, info.maxBlockWeight[i]);
+		cs.initialize(s, t);
+		
+		timer.start();
+		flow_algo.exhaustFlow(cs);
+		timer.stop();
+		std::cout << V(cs.flowValue) << std::endl;
+		
+		{
+			// test if flow is maximal
+			BitVector vis(hg.numNodes()), he_vis(hg.numHyperedges()), he_flow_sending_vis(hg.numHyperedges());
+			std::queue<Node> q;
+			
+			size_t n_vis = 0;
+			auto visit = [&](Node v) {
+				assert(!cs.n.isTarget(v));
+				if (!vis[v]) {
+					vis.set(v);
+					q.push(v);
+					n_vis++;
+				}
+			};
+			
+			for (auto& sp : cs.sourcePiercingNodes)
+				visit(sp.node);
+			
+			while (!q.empty()) {
+				const Node u = q.front(); q.pop();
+				for (InHeIndex inc_u_iter : hg.incidentHyperedgeIndices(u)) {
+					const auto& inc_u = hg.getInHe(inc_u_iter);
+					const Hyperedge e = inc_u.e;
+					if (!he_vis[e]) {
+						if (!hg.isSaturated(e) || hg.flowReceived(inc_u) > 0) {
+							he_vis.set(e);
+							for (auto& pv : hg.pinsOf(e)) {
+								visit(pv.pin);
+							}
+						} else if(!he_flow_sending_vis[e]) {
+							he_flow_sending_vis.set(e);
+							for (auto& pv : hg.pinsOf(e)) {
+								if (hg.flowSent(pv) > 0) {
+									visit(pv.pin);
+								}
+							}
+						}
+					}
+				}
+			}
+			assert(n_vis < hg.numNodes());
+		}
+		
+		timer.report(std::cout);
+		return cs.flowValue;
+	}
+}
+
+int main(int argc, const char* argv[]) {
+	if (argc < 2 || argc > 3)
+		throw std::runtime_error("Usage: ./FlowTester hypergraphfile");
+	std::string hgfile = argv[1];
+	int f1 = whfc::runSnapshotTester<whfc::BidirectionalDinic>(hgfile);
+	int f2 = whfc::runSnapshotTester<whfc::Dinic>(hgfile);
+	assert(f1 == f2);
+	int f3 = whfc::runSnapshotTester<whfc::ScalingDinic>(hgfile);
+	assert(f2 == f3);
+	return 0;
+}
