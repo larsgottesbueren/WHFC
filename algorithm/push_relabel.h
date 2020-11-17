8ed30c3508219d3d694361399480fab34225f1eb
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-11-17 14:06:18 +0100

    PushRelabel: implement dischargeNode

--- algorithm/push_relabel.h
+++ algorithm/push_relabel.h
@@ -117,7 +117,41 @@ namespace whfc {
 		}
 
 		void dischargeNode(Node u) {
+			assert(hg.degree(u) > 0);
+			const Hyperedge num_nodes(hg.numNodes());	// TaggedInteger conversion...
+			while (excess[u] > 0) {
+				InHe& inc_he = hg.getInHe(current_hyperedge[u]);
+				const Node e_node(inc_he.e + num_nodes);
+				Flow residual = hg.residualCapacity(inc_he.e) + hg.absoluteFlowReceived(inc_he);
+				if (residual > 0 && level[e_node] == level[u] + 1) {
+					// push p4 and p2
+					if (excess[e_node] == 0) {
+						assert(level[e_node] < max_level);
+						active_vertices_and_edges.push_back(e_node);
+					}
+					residual = std::min(residual, excess[u]);
+					excess[e_node] += residual;
+					excess[u] -= residual;
+					inc_he.flow += residual;
+					// update flow value of hyperedge due to p4. First consume p2 completely, then p4
+					hg.flow(inc_he.e) += std::max(0, residual - hg.absoluteFlowReceived(inc_he));
 
+					assert(hg.flow(inc_he.e) == in_flow(e_node));
+					assert(excess[e_node] == in_flow(e_node) - out_flow(e_node));	// these break running time guarantees in debug mode
+				} else {
+					// don't advance iterator if pushed
+					if (++current_hyperedge[u] == hg.endIndexHyperedges(u)) {
+						// relabel
+						uint32_t min_level = std::numeric_limits<uint32_t>::max();
+						for (const InHe& inc_he2 : hg.hyperedgesOf(u)){
+							if (hg.residualCapacity(inc_he2.e) + hg.absoluteFlowReceived(inc_he2) > 0) {
+								min_level = std::min(min_level, level[inc_he2.e + num_nodes]);
+							}
+						}
+						current_hyperedge[u] = hg.beginIndexHyperedges(u);
+					}
+				}
+			}
 		}
 
 		void dischargeEdge(Hyperedge e, Node e_node) {
@@ -134,6 +168,37 @@ namespace whfc {
 
 		void finish(CutterState<Type>& cs) {
 
+		}
+
+		std::pair<Flow,Flow> local_flow(const Node u) const {
+			Flow in_flow = 0, out_flow = 0;
+			if (u < hg.numNodes()) {
+				for (InHe& inc_he : hg.hyperedgesOf(u)) {
+					if (inc_he.flow > 0) {
+						out_flow += inc_he.flow;
+					} else {
+						in_flow -= inc_he.flow;
+					}
+				}
+			} else {
+				for (Pin& pin : hg.pinsOf(Hyperedge(u - hg.numNodes()))) {
+					const Flow f = hg.getInHe(pin).flow;
+					if (f > 0) {
+						out_flow += f;
+					} else {
+						in_flow -= f;
+					}
+				}
+			}
+			return std::make_pair(in_flow, out_flow);
+		}
+
+		Flow in_flow(const Node u) const {
+			return local_flow(u).first;
+		}
+
+		Flow out_flow(const Node u) const {
+			return local_flow(u).second;
 		}
 
 		TimeReporter timer;
